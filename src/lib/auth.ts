748cc5d048ab9e1ee3a255bba89b8e87e5e0ad82
vnniciusg <90412694+vnniciusg@users.noreply.github.com>
2023-08-03 13:40:30 -0300

    fix: estruturação das pastas

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,32 @@
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
     }),
   ],
 };
